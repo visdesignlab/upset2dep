d71326008c6ffae36fa716a55ec4fd4675885364
Kiran Gadhave <kirangadhave2@gmail.com>
2020-05-24 19:54:11 -0600

    Resetting for lerna

--- packages/upset-core/src/Proc/getExclusiveIntersections.ts
+++
@@ -1,5 +0,0 @@
-import { Sets } from '../Types/Model';
-
-export function getExclusiveIntersections<T>(sets: Sets<T>) {
-  if (sets) console.log('Hello');
-}
--- packages/upset-core/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { loadMovies } from './loadFile';
-
-loadMovies();
