8adae3542bfcdeb6f47fa6583324497d715d5084
Kiran Gadhave <kirangadhave2@gmail.com>
2020-05-25 13:57:11 -0600

    Added the front end

---
+++ packages/web-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {Thing} from 'upset-react';
+
+function App() {
+  return (
+    <div className="App">
+        <Thing/>
+    </div>
+  );
+}
+
+export default App;
